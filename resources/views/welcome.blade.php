7963e5a233b5356edb136da23c85971d4da5a812
HlaingTinHtun <hlaingtinhtun@gmail.com>
2017-05-15 16:23:48 +0630

    [Test Broadcast] Make test file to broadcast event

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,52 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+</head>
+<body>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<h1>Welcome</h1>
+
+<ul id="users">
+    <li v-repeat="user: users">@{{ user.name }}</li>
+</ul>
+<script src="https://js.pusher.com/4.0/pusher.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/0.12.9/vue.min.js"></script>
+<script>
+    //            (function(){
+    //                var pusher = new Pusher('02c5b12fd179bf67953d', {
+    //                    encrypted: true
+    //                });
+    //
+    //                var channel = pusher.subscribe('test');
+    //
+    //                channel.bind('App\\Events\\UserHasRegistered', function(data) {
+    //                    console.log(data.name);
+    //                });
+    //
+    //            })();
+    new Vue({
+        el: '#users',
+
+        data:{
+            users:[]
+        },
+        ready:function(){
+            var pusher = new Pusher('02c5b12fd179bf67953d', {
+                encrypted: true
+            });
+
+            pusher.subscribe('test')
+                    .bind('App\\Events\\UserHasRegistered', this.addUser);
+        },
+        methods:{
+            addUser: function(user){
+                this.users.push(user);
+            }
+        }
+    })
+
+</script>
+</body>
+</html>
